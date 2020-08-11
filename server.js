7df41f0f851686b24466d1ab750a9157a634f0a0
George Flores <georgeflores886@gmail.com>
2020-08-11 10:47:48 -0400

    added conditional to body and body-parser

--- server.js
+++ server.js
@@ -1,47 +1,83 @@
 const express = require('express');
-const app =  express();
+const app = express();
+const bodyParser = require('body-parser')
 const PORT = 3000;
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
     res.send('hello world')
 })
 
 app.get('/status', (req, res) => {
-    res.status(200).json({message:'ok', status: 200})
+    res.status(200).json({ message: 'ok', status: 200 })
 })
 
 app.post('/signup', (req, res, next) => {
-    next(new Error('test'));
-    res.status(200).json({message:'ok', status: 200})
-} )
+    if (!req.body) {
+        res.status(400).json({ message: 'invalid body', status: 400 })
+    } else {
+        res.status(200).json({ message: 'ok', status: 200 })
+
+    }
+})
 
 app.post('/login', (req, res) => {
-    res.status(200).json({message:'ok', status: 200})
+    if (!req.body) {
+        res.status(400).json({ message: 'invalid body', status: 400 })
+    } else {
+        res.status(200).json({ message: 'ok', status: 200 })
+    }
 })
 
 app.post('/logout', (req, res) => {
-    res.status(200).json({message:'ok', status: 200})
+    if (!req.body) {
+        res.status(400).json({ message: 'invalid body', status: 400 })
+    } else {
+        res.status(200).json({ message: 'ok', status: 200 })
+
+    }
+})
+
+app.post('/token', (req, res) => {
+    if (!req.body || !req.body.refreshToken) {
+        res.status(400).json({ message: 'invalid body', status: 400 })
+    } else {
+        const { refreshToken } = req.body
+        res.status(200).json({ message: `refresh token requested for token: ${refreshToken}`, status: 200 })
+    }
 })
 
 app.post('/forgot-password', (req, res) => {
-    res.status(200).json({message:'ok', status: 200})
+    if (!req.body || !req.body.email) {
+        res.status(400).json({ message: 'invalid body', status: 400 })
+    } else {
+        const { email } = req.body
+        res.status(200).json({ message: `forgot password request for email: ${email}`, status: 200 })
+    }
 })
 
 app.post('/reset-password', (req, res) => {
-    res.status(200).json({message:'ok', status: 200})
+    if (!req.body || !req.body.email) {
+        res.status(400).json({ message: 'invalid body', status: 400 })
+    } else {
+        const { email } = req.body
+        res.status(200).json({ message: `password reset request for email: ${email}`, status: 200 })
+    }
 })
 
 //catch all other routes
 app.use((req, res) => {
-    res.status(404).json({message: '404 NOT FOUND', status: 404})
-}) 
+    res.status(404).json({ message: '404 NOT FOUND', status: 404 })
+})
 //handle errors
 app.use((req, res, next) => {
     console.log(error)
-    res.status(error.status || 500).json({error: error.message, status: 500})
-}) 
+    res.status(error.status || 500).json({ error: error.message, status: 500 })
+})
 
-app.listen(PORT, () =>{
+app.listen(PORT, () => {
     console.log('Listening on port ' + PORT)
 })
 
