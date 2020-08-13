a0406e6b626dba1c23e8895ae70f287dfb995e32
George Flores <georgeflores886@gmail.com>
2020-08-13 09:13:26 -0400

    implemented bcrypt in userModel

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-
+const bcrypt = require('bcrypt')
 const {Schema } =  mongoose;
 
 const UserSchema =  new Schema ({
@@ -25,8 +25,16 @@ const UserSchema =  new Schema ({
 })
 
 UserSchema.pre('save', async function (next) {
+    const hash = await bcrypt.hash(this.password, 10);
+    this.password.hash;
     next();
 })
+
+UserSchema.methods.isValidPassword = async function (password) {
+    const user = this;
+    const compare =  await bcrypt.compare(password, user.password);
+    return compare;
+}
 
 const UserModel =  mongoose.model('user', UserSchema);
 
