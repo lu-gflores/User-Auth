2e56637f00cd3dacdcedd9a23ca814be2871f07d
George Flores <georgeflores886@gmail.com>
2020-08-14 11:09:39 -0400

    updated reset and forgot password routes

--- routes/password.js
+++ routes/password.js
@@ -18,6 +18,7 @@ const smtpTransport = nodemailer.createTransport({
 const handlebarsOptions = {
     viewEngine: {
         extName: '.hbs',
+        defaultLayout: null,
         partialsDir: './templates/',
         layoutsDir: './templates/'
     },
@@ -29,21 +30,46 @@ smtpTransport.use('compile', hbs(handlebarsOptions))
 
 const router = express.Router() //new express router instance
 
-router.post('/forgot-password', (req, res) => {
+router.post('/forgot-password', async (req, res) => {
     if (!req.body || !req.body.email) {
         res.status(400).json({ message: 'invalid body', status: 400 })
     } else {
-        const { email } = req.body
-        res.status(200).json({ message: `forgot password request for email: ${email}`, status: 200 })
+        const  userEmail  = req.body.email
+        //who is this email from?
+        const emailOptions = {
+            to: userEmail,
+            from: email,
+            template: 'forgot-password',
+            subject: 'Zenva PHASER MMO Password Reset',
+            context: {
+                name: 'john',
+                url: `http://localhost:${process.env.PORT || 3000}`
+            }
+        }
+        await smtpTransport.sendMail(emailOptions)
+
+        res.status(200).json({ message: 'An email has been sent to your email address. Reset link is valid for 10 mins.', status: 200 })
     }
 })
 
-router.post('/reset-password', (req, res) => {
+router.post('/reset-password', async (req, res) => {
     if (!req.body || !req.body.email) {
         res.status(400).json({ message: 'invalid body', status: 400 })
     } else {
-        const { email } = req.body
-        res.status(200).json({ message: `password reset request for email: ${email}`, status: 200 })
+        const userEmail = req.body.email
+
+        const emailOptions = {
+            to: userEmail,
+            from: email,
+            template: 'reset-password',
+            subject: 'Zenva PHASER MMO Password Reset Confirmation',
+            context: {
+                name: 'john',
+            }
+        }
+        await smtpTransport.sendMail(emailOptions)
+
+        res.status(200).json({ message: 'Password updated', status: 200 })
     }
 })
 module.exports = router;
