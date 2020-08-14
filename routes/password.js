e74d42e6052de910817ac51a6cdd418629205218
George Flores <georgeflores886@gmail.com>
2020-08-14 11:30:07 -0400

    modified resetting password

--- routes/password.js
+++ routes/password.js
@@ -6,7 +6,7 @@ const crypto = require('crypto')
 const UserModel = require('../models/UserModel')
 
 //storing email and password
-const email = process.env.EMAIL; 
+const email = process.env.EMAIL;
 const password = process.env.PASSWORD;
 
 const smtpTransport = nodemailer.createTransport({
@@ -33,10 +33,10 @@ smtpTransport.use('compile', hbs(handlebarsOptions))
 const router = express.Router() //new express router instance
 
 router.post('/forgot-password', async (req, res) => {
-    const  userEmail  = req.body.email;
-    const user =  await UserModel.findOne({email: userEmail});
-    if(!user) {
-        res.status(400).json( {message: 'invalid email', status: 400})
+    const userEmail = req.body.email;
+    const user = await UserModel.findOne({ email: userEmail });
+    if (!user) {
+        res.status(400).json({ message: 'invalid email', status: 400 })
         return;
     }
 
@@ -45,7 +45,7 @@ router.post('/forgot-password', async (req, res) => {
     const token = buffer.toString('hex');
 
     //update user reset pass
-    await UserModel.findByIdAndUpdate({ _id:user._id}, {resetToken: token, resetTokenExp: Date.now() + 60000})
+    await UserModel.findByIdAndUpdate({ _id: user._id }, { resetToken: token, resetTokenExp: Date.now() + 60000 })
 
     const emailOptions = {
         to: userEmail,
@@ -54,33 +54,49 @@ router.post('/forgot-password', async (req, res) => {
         subject: 'Zenva PHASER MMO Password Reset',
         context: {
             name: 'john',
-            url: `http://localhost:${process.env.PORT || 3000}`
+            url: `http://localhost:${process.env.PORT || 3000}?token=${token}`
         }
     }
     await smtpTransport.sendMail(emailOptions)
 
     res.status(200).json({ message: 'An email has been sent to your email address. Reset link is valid for 10 mins.', status: 200 })
-  
+
 })
 
 router.post('/reset-password', async (req, res) => {
-    if (!req.body || !req.body.email) {
-        res.status(400).json({ message: 'invalid body', status: 400 })
-    } else {
-        const userEmail = req.body.email
+    const userEmail = req.body.email
+    const user = await UserModel.findOne({ resetToken: req.body.token, resetTokenExp: { $gt: Date.now() }, email: userEmail })
 
-        const emailOptions = {
-            to: userEmail,
-            from: email,
-            template: 'reset-password',
-            subject: 'Zenva PHASER MMO Password Reset Confirmation',
-            context: {
-                name: 'john',
-            }
-        }
-        await smtpTransport.sendMail(emailOptions)
-
-        res.status(200).json({ message: 'Password updated', status: 200 })
+    if (!user) {
+        res.status(400).json({ message: 'invalid token', status: 400 })
+        return;
     }
+
+    //checking if password is provided and matches verified password
+    if(!req.body.password || !req.body.verifiedPassword || req.body.password !== req.body.verifiedPassword) {
+        res.status(400).json({ message: 'passwords do not match.', status: 400 })
+        return;
+    }
+
+    //modifing user model
+    user.password =  req.body.password;
+    user.resetToken = undefined;
+    user.resetTokenExp = undefined;
+    await user.save();
+
+
+    const emailOptions = {
+        to: userEmail,
+        from: email,
+        template: 'reset-password',
+        subject: 'Zenva PHASER MMO Password Reset Confirmation',
+        context: {
+            name: user.username,
+        }
+    }
+    await smtpTransport.sendMail(emailOptions)
+
+    res.status(200).json({ message: 'Password updated', status: 200 })
+
 })
 module.exports = router;
